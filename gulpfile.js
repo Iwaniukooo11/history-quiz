624e656f6569d288eab9647f82d24316029ce48d
Mateusz Iwaniuk <mateusz.iwaniuk11@gmail.com>
2019-05-07 22:43:56 +0200

    -change gulpfile(add waiter for good working) -rebuild HTML for BEM -rebuild CSS -add different SCSS

--- gulpfile.js
+++ gulpfile.js
@@ -10,44 +10,59 @@ const {
   sass = require('gulp-sass'),
   autoprefixer = require('gulp-autoprefixer'),
   browserSync = require('browser-sync').create(),
-  cleaner = require('gulp-clean');
+  cleaner = require('gulp-clean'),
+  wait = require('gulp-wait')
 
 sass.compiler = require('node-sass');
 
+function hello() {
+  console.log('Hello!');
+}
+exports.hello = hello
+
 function copy() {
   return src('./src/html/*.html')
+    .pipe(wait(100))
     .pipe(dest('./dist'))
 }
 
-function assets(){
+function assets() {
   return src('./src/assets/**/*')
-  .pipe(dest('./dist/assets'))
+    .pipe(wait(100))
+    .pipe(dest('./dist/assets'))
 }
 
 function js() {
   return src('./src/js/*.js')
+    .pipe(wait(100))
     .pipe(minifyJS({
-      ext:{
-        min:'.min.js'
-    }
+      ext: {
+        min: '.min.js'
+      }
     }))
     .pipe(dest('./dist/assets/js'))
 }
 
 function css() {
   return src('./src/scss/*.scss')
+    .pipe(wait(100))
     .pipe(sass().on('error', sass.logError))
+    .pipe(sass({
+      includePaths: ['node_modules']
+    }))
     .pipe(autoprefixer({
-      browsers: ['last 2 versions'],
+      browsers: ['last 1 versions'],
       cascade: false
     }))
     .pipe(minifyCSS({
       compatibility: 'ie8'
     }))
+
     .pipe(dest('./dist/assets/css/'))
 }
 
 function reload() {
+
   browserSync.init({
     server: {
       baseDir: "./dist"
@@ -55,15 +70,19 @@ function reload() {
   });
 }
 
-function watcher(){
+function watcher() {
   watch('./src/html/**/*.html').on('change', series(copy, browserSync.reload))
   watch('./src/js/**/*.js').on('change', series(js, browserSync.reload))
-  watch('./src/scss/**/*.scss').on('change', series(css, browserSync.reload))
+  watch('./src/scss/**/*.scss').on('change', series(copy, css, browserSync.reload))
   watch('./src/assets/**/*').on('change', assets);
 }
 
-function clean(){
-  return src('./dist', {read: false, allowEmpty: true})
+function clean() {
+  return src('./dist', {
+      read: false,
+      allowEmpty: true
+    })
+    .pipe(wait(100))
     .pipe(cleaner());
 }
 
