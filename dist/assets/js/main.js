19fccec2fa9d643119b9432474bb0663c8dc7275
Mateusz Iwaniuk <mateusz.iwaniuk11@gmail.com>
2019-05-09 19:15:42 +0200

    remove warp-me form some elements add clicking answer

--- dist/assets/js/main.js
+++ dist/assets/js/main.js
@@ -1,1 +1,24 @@
-console.log('test');
+console.log('test1');
+
+
+class Game {
+    constructor() {
+        this.answers = document.querySelectorAll('.exercises__answers__answer')
+
+        this.answers.forEach(e => {
+            e.addEventListener('click', el => {
+                if (!el.target.classList.contains('onBrown')) {
+                    el.target.parentNode.childNodes.forEach(e => {
+                        if (e.tagName == 'LI')
+                            e.classList.remove('onBrown')
+                    })
+                }
+                el.target.classList.toggle('onBrown')
+            })
+        })
+    }
+    changeInline(element, atribute, value) {
+        element.style[atribute] = value
+    }
+}
+const g = new Game()
